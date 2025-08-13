6a514f391e9c046a8245ac1a06a2cf2acee379f7
Kushal2205a <introvertedspacemine2@gmail.com>
2025-08-13 06:00:16 +0530

    Made changes to authenticate_gdrive()

--- app.py
+++ app.py
@@ -33,7 +33,7 @@ def authenticate_gdrive():
                 json.dump(creds_dict, f)
 
             flow = InstalledAppFlow.from_client_secrets_file("temp_credentials.json", SCOPES)
-            creds = flow.run_local_server(port=8080)
+            creds = flow.run_console()
         with open("token.json", "w") as token:
             token.write(creds.to_json())
     return creds
